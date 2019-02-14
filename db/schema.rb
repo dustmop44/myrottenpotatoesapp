453512aa489a2e085f1d33027e0fec32ce9a0c04
EC2 Default User <ec2-user@ip-172-31-13-85.ec2.internal>
2019-02-14 17:54:47 +0000

    add image to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_174802) do
+ActiveRecord::Schema.define(version: 2019_02_14_175418) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_174802) do
     t.string "name"
     t.string "avatar"
     t.string "provider"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
