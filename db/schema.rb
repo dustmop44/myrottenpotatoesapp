b4492096805f58c4bbd093509e424e8489cb897b
EC2 Default User <ec2-user@ip-172-31-13-85.ec2.internal>
2019-02-14 17:49:09 +0000

    add provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_161653) do
+ActiveRecord::Schema.define(version: 2019_02_14_174802) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_161653) do
     t.string "uid"
     t.string "name"
     t.string "avatar"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
